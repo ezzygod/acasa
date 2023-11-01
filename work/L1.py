67fef5dc7958b96cdcb42d70755c735cf9a9162a
ezzygod <147803799+ezzygod@users.noreply.github.com>
2023-11-01 22:04:50 +0200

    Delete work directory

--- work/L1.py
+++
@@ -1,9 +0,0 @@
-import math
-ip = None
-
-
-catete = input("Dati valorile catetelor: ")
-cateta, cateta2 = map(float, catete.split())
-
-ip = math.sqrt(cateta**2 + cateta2**2)
-print("Lungimea ipotenuzei este: ", round(ip, 2)
