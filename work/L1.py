a90c9c84252fcbe2c1d5eae2204c9b71cafca60b
ezzygod <ezz4life1k@gmail.com>
2023-11-01 22:03:31 +0200

    v2

--- work/L1.py
+++ work/L1.py
@@ -7,5 +7,3 @@ cateta, cateta2 = map(float, catete.split())
 
 ip = math.sqrt(cateta**2 + cateta2**2)
 print("Lungimea ipotenuzei este: ", round(ip, 2))
-git config user.email "ezz4life1k@gmail.com"
-git config user.name "ezzygod"
