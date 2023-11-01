b5ce831b5fb80f5acf0bacf0dc6394179c2d3ac1
ezzygod <ezz4life1k@gmail.com>
2023-11-01 22:04:22 +0200

    v3

--- work/L1.py
+++ work/L1.py
@@ -6,4 +6,4 @@ catete = input("Dati valorile catetelor: ")
 cateta, cateta2 = map(float, catete.split())
 
 ip = math.sqrt(cateta**2 + cateta2**2)
-print("Lungimea ipotenuzei este: ", round(ip, 2))
+print("Lungimea ipotenuzei este: ", round(ip, 2)
