0da73a87d320e8ad335baf9fb6039076206bfe62
ezzygod <ezz4life1k@gmail.com>
2023-11-01 22:30:49 +0200

    vF

---
+++ lucr2.py
@@ -0,0 +1,9 @@
+parolacor = "cola"
+
+while True:
+    parola = input("Introduceti parola pentru a merge mai departe: ")
+    if parola ==  parolacor:
+        print("Parola este corecta !")
+        break
+    else:
+        print("Incercati din nou !")
